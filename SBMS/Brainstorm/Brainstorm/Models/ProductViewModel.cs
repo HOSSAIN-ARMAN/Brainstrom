6afba46e77135bcebbeb02b96023583d3b0bebfe
HOSSAIN-ARMAN <hossainarman4389@gmail.com>
2020-01-21 00:20:30 +0600

    .net StockManagmentProject

---
+++ SBMS/Brainstorm/Brainstorm/Models/ProductViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Brainstorm.Model.Model;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Brainstorm.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Code Field Cannot be empty")]//annotations
+        [MaxLength(4, ErrorMessage = "Max length is 4")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Code must be numeric")]
+        [Display(Name = "Product Code:")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name Cannot be empty")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Use Characters only")]
+        [Display(Name = "Product Name:")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Select a Category")]
+        [Display(Name = "Category:")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid")]
+        [Display(Name = "Reorder Level:")]
+        public int ReorderLevel { get; set; }
+        [Display(Name = "Description:")]
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
+        public List<SelectListItem> CategorySelectListItems { get; set; }
+    }
+}
---
+++ SBMS/Brainstorm/Brainstorm/Models/StockViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Brainstorm.Models
+{
+    public class StockViewModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Product { get; set; }
+        public string Category { get; set; }
+        public int ReorderLevel { get; set; }
+        public DateTime Expdate { get; set; }
+        public int OpeningBalance { get; set; }
+        public int In { get; set; }
+        public int Out { get; set; }
+        public int ClosingBalance { get; set; }
+    }
+}
